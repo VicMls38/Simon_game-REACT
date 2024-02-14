a4b4d3ca13092e8a0f0c7ec5dcc327c5ac441076
Victor MORALES <victor.morales@outlook.fr>
2024-02-14 16:13:42 +0100

    add manifest config

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,43 @@ export default defineConfig({
       registerType: 'autoUpdate',
       devOptions: {
         enabled: true
-      }
+      },
+      manifest: {
+        theme_color: "#f69435",
+        background_color: "#f69435",
+        display: "browser",
+        scope: "/",
+        start_url: "/",
+        name: "simon",
+        short_name: "simon",
+        icons: [
+            {
+                src: "/icon-144x144.png",
+                sizes: "144x144",
+                type: "image/png"
+            },
+            {
+                src: "/icon-192x192.png",
+                sizes: "192x192",
+                type: "image/png"
+            },
+            {
+                src: "/icon-256x256.png",
+                sizes: "256x256",
+                type: "image/png"
+            },
+            {
+                src: "/icon-384x384.png",
+                sizes: "384x384",
+                type: "image/png"
+            },
+            {
+                src: "/icon-512x512.png",
+                sizes: "512x512",
+                type: "image/png"
+            }
+        ]
+    }
     })
   ],
 })
